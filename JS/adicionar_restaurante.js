b6a38ee0d1592799d5731607907c0232b0fcecf8
Edioelson Júnior A. B. Teixeira <ejrabt1@gmail.com>
2024-04-29 10:33:55 -0300

    feat: Add a character selector
    Agora, só é possível digitar os caracteres permitidos.

--- JS/adicionar_restaurante.js
+++ JS/adicionar_restaurante.js
@@ -19,4 +19,17 @@ document.getElementById('themeSwitcher').addEventListener('click', function() {
     }
 })
 
-// TODO: Adicionar seletor de caracteres
+// Seletor de caracteres
+const cep = document.getElementById('cepRestaurante');
+const allowedKeys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '.'];
+
+cep.addEventListener('keydown', function(ev) {
+    ev.preventDefault();
+    if (allowedKeys.includes(ev.key)) {
+        cep.value += ev.key;
+        return;
+    }
+    if (ev.key === 'Backspace') {
+        cep.value = cep.value.slice(0, -1);
+    }
+});
