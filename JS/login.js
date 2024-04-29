f9d14ecc2bd542109d0ba1c81be12d7ee6a80e8b
Edioelson Júnior A. B. Teixeira <ejrabt1@gmail.com>
2024-04-29 09:43:39 -0300

    feat: Add login feature
    Add login feature with HTML, CSS and JS

---
+++ JS/login.js
@@ -0,0 +1,20 @@
+const root = document.querySelector(':root');
+const body = document.querySelector('body');
+
+document.getElementById('themeSwitcher').addEventListener('click', function () {
+    if (body.dataset.theme === 'light') {
+        root.style.setProperty('--background-color-form', '#1f1f1f')
+        root.style.setProperty('--color-btn-red', '#800000');
+        root.style.setProperty('--h2-color', '#cccccc');
+        root.style.setProperty('--background-img', 'url(../IMG/background-dark-img.jpg)');
+        root.style.setProperty('--hover-color', '#f00');
+        body.dataset.theme = 'dark';
+    } else {
+        root.style.setProperty('--background-color-form', '#fff')
+        root.style.setProperty('--color-btn-red', '#f00');
+        root.style.setProperty('--h2-color', '#5c4033');
+        root.style.setProperty('--background-img', 'url(../IMG/Imagem_de_fundo.jpeg)');
+        root.style.setProperty('--hover-color', '#800000');
+        body.dataset.theme = 'light';
+    }
+});
